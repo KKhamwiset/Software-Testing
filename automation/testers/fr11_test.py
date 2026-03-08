0fe3fa2304ca8cbe2fb5478995c28ba92630dc5b
KKhamwiset <kritsakorn224@gmail.com>
2026-03-08 22:35:47 +0700

    Completed and tested

--- automation/testers/fr11_test.py
+++ automation/testers/fr11_test.py
@@ -14,29 +14,29 @@ def run_fr11_test():
         print(f"Navigating to {url}login")
         driver.get(url + "login")
             
-        try:
-            wait = WebDriverWait(driver, 15)
-            user = os.getenv("admin_username","JD")
-            passwd = os.getenv("admin_password","FRtest1_123")
-            field =[
-                ("user_email", user),
-                ("password" , passwd),
-            ]
-            for id,value in field:
-                print(f"Waiting for {id}...")
-                input_el = wait.until(EC.visibility_of_element_located((By.ID, id)))
-                input_el.clear()
-                input_el.click()
-                input_el.send_keys(value)
-                print(f"Entered {id} successfully.")
-                time.sleep(0.3) 
-            id = "login"
-            log_in_button = wait.until(EC.element_to_be_clickable((By.ID, id)))
-            wait.until(lambda d: d.find_element(By.ID, id).is_enabled())
-            log_in_button.click()
-            print("Log in succesfully")
-            time.sleep(2)
-            
-        except Exception as e:
-            print(f"Test failed: {str(e)}")
+    try:
+        wait = WebDriverWait(driver, 15)
+        user = os.getenv("admin_username","JD")
+        passwd = os.getenv("admin_password","FRtest1_123")
+        field =[
+            ("user_email", user),
+            ("password" , passwd),
+        ]
+        for id,value in field:
+            print(f"Waiting for {id}...")
+            input_el = wait.until(EC.visibility_of_element_located((By.ID, id)))
+            input_el.clear()
+            input_el.click()
+            input_el.send_keys(value)
+            print(f"Entered {id} successfully.")
+            time.sleep(0.3) 
+        id = "login"
+        log_in_button = wait.until(EC.element_to_be_clickable((By.ID, id)))
+        wait.until(lambda d: d.find_element(By.ID, id).is_enabled())
+        log_in_button.click()
+        print("Log in succesfully")
+        time.sleep(2)
+        
+    except Exception as e:
+        print(f"Test failed: {str(e)}")
 
